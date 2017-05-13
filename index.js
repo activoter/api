8ac7f02533b6859bfaf1c0ecde01f649a6d042c6
finix-rsmith <rsmith@finixcreative.com>
2017-05-12 19:18:49 -0700

    DB config updates

--- index.js
+++ index.js
@@ -8,10 +8,13 @@ let db = mongoose.connection
 let api = require('./lib/routes/api')
 
 // DB Config
-const DEV_DB = 'mongodb://localhost:27017/activoter'
-let mongodbUri = process.env.MONGODB_URI || DEV_DB
+const TEST_DB = 'mongodb://localhost/activoter'
+let dbUri = process.env.MONGODB_URI || process.env.MONGOHQ_URL || TEST_DB
 
-mongoose.connect(mongodbUri)
+mongoose.connect(dbUri, (err, data) => {
+	if (err) console.log (`ERROR connecting to: ${dbUri}. ${err}`)
+	console.log (`Succeeded connected to: ${dbUri}`);
+})
 
 db.on('error', console.error.bind(console, 'connection error:'))
 
@@ -20,20 +23,18 @@ db.once('open', function() {
 })
 
 // API Config
-app.use(bodyParser.urlencoded({extended: true}))
+app.use(bodyParser.urlencoded({ extended: true }))
 app.use(bodyParser.json())
 app.use(express.static(__dirname))
 app.use('/', api)
 
 // Root
-/*
 app.get('/', (req, res) => {
-	res.sendfile(path.join(__dirname + '/index.html'))
+	res.sendFile(path.join(__dirname + '/lib/index.html'))
 })
-*/
 
 // Startup
-let port = 9999
+let port = process.env.PORT || 9999
 app.listen(port, () => {
 	console.log('Listening on Port ' + port)	
 })
