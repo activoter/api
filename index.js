d2df4c9dab49e51f0cc8a3666859276b8aa65f22
finix-rsmith <rsmith@finixcreative.com>
2017-05-23 17:18:32 -0700

    User auth with Passport

--- index.js
+++ index.js
@@ -3,6 +3,10 @@ let express = require('express')
 let app = express()
 let path = require('path')
 let bodyParser = require('body-parser')
+let passport = require('passport')
+let cookieSession = require('cookie-session')
+let cookieParser = require('cookie-parser')
+let LocalStrategy = require('passport-local').Strategy
 let mongoose = require('mongoose')
 let db = mongoose.connection
 let api = require('./lib/routes/api')
@@ -33,12 +37,56 @@ app.use(allowCrossDomain)
 app.use(bodyParser.urlencoded({ extended: true }))
 app.use(bodyParser.json())
 app.use(express.static(__dirname))
+app.use(cookieSession({
+	name: 'session',
+	keys: ['tricky'],
+	maxAge: 24 * 60 * 60 * 1000 // 24 hours
+}))
+app.use(cookieParser())
+app.use(passport.initialize())
+app.use(passport.session())
 app.use('/', api)
+
+passport.use(new LocalStrategy((username, password, done) => {
+	User.findOne({ username: username }, (err, user) => {
+		if (err) return done(err)
+		if (!user) {
+			return done(null, false, { message: 'Incorrect username.' })
+		}
+		if (!user.validPassword(password)) {
+			return done(null, false, { message: 'Incorrect password.' })
+		}
+		return done(null, user)
+	})
+}))
+
+passport.serializeUser(function(user, done) {
+	done(null, user.id)
+})
+
+passport.deserializeUser(function(id, done) {
+	User.findById(id, function(err, user) {
+		done(err, user)
+	})
+})
 
 // Home route
 app.get('/', (req, res) => {
 	res.sendFile(path.join(__dirname + '/public/index.html'))
 })
+
+// Login
+app.post(
+	'/login',
+	passport.authenticate(
+		'local',
+		{
+			successRedirect: '/',
+			failureRedirect: '/login',
+			failureFlash: true
+		}
+	)
+)
 
 // Startup
 let port = process.env.PORT || 9999
