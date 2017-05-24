b6b079ab9db6c2513d512e005226ce5f2c4858ba
finix-rsmith <rsmith@finixcreative.com>
2017-05-23 17:42:34 -0700

    Passport credential settings

--- index.js
+++ index.js
@@ -47,11 +47,11 @@ app.use(passport.initialize())
 app.use(passport.session())
 app.use('/', api)
 
-passport.use(new LocalStrategy((username, password, done) => {
-	User.findOne({ username: username }, (err, user) => {
+passport.use(new LocalStrategy((email, password, done) => {
+	User.findOne({ email: email }, (err, user) => {
 		if (err) return done(err)
 		if (!user) {
-			return done(null, false, { message: 'Incorrect username.' })
+			return done(null, false, { message: 'Incorrect email.' })
 		}
 		if (!user.validPassword(password)) {
 			return done(null, false, { message: 'Incorrect password.' })
@@ -61,7 +61,7 @@ passport.use(new LocalStrategy((username, password, done) => {
 }))
 
 passport.serializeUser(function(user, done) {
-	done(null, user.id)
+	done(null, user._id)
 })
 
 passport.deserializeUser(function(id, done) {
